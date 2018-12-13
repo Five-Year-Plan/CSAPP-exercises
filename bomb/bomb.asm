03b225dbf45ddf24648027791024c7635ffa8e7d
factotumKS <1312005925@qq.com>
2018-12-13 14:22:31 +0800

    solve phase2 in bomb lab

--- bomb/bomb.asm
+++ bomb/bomb.asm
@@ -302,7 +302,7 @@ Disassembly of section .text:
   /400e3f:	e8 80 07 00 00       	callq  4015c4 <phase_defused>
   400e44:	bf a8 23 40 00       	mov    $0x4023a8,%edi
   400e49:	e8 c2 fc ff ff       	callq  400b10 <puts@plt>
-  400e4e:	e8 4b 06 00 00       	callq  40149e <read_line>
+  400e4e:	e8 4b 06 00 00       	callq  40149e <read_line> # 类似输入
   400e53:	48 89 c7             	mov    %rax,%rdi
   400e56:	e8 a1 00 00 00       	callq  400efc <phase_2>
   400e5b:	e8 64 07 00 00       	callq  4015c4 <phase_defused>
@@ -343,7 +343,7 @@ Disassembly of section .text:
   400ede:	90                   	nop
   400edf:	90                   	nop
 
-0000000000400ee0 <phase_1>:
+0000000000400ee0 <phase_1>: # "Border relations with Canada have never been better."
   400ee0:	48 83 ec 08          	sub    $0x8,%rsp        # 分配8个字节的空间
   400ee4:	be 00 24 40 00       	mov    $0x402400,%esi   # 传参数(在main中使用了%rdi保存输入)，肯定是答案
   400ee9:	e8 4a 04 00 00       	callq  401338 <strings_not_equal>
@@ -353,28 +353,28 @@ Disassembly of section .text:
   400ef7:	48 83 c4 08          	add    $0x8,%rsp        # 归还空间
   400efb:	c3                   	retq   
 
-0000000000400efc <phase_2>:
-  400efc:	55                   	push   %rbp
-  400efd:	53                   	push   %rbx
+0000000000400efc <phase_2>: # "1 2 4 8 16 32" 不应该看read_six_number，比较复杂，观察phase_1就可以猜出答案
+  400efc:	55                   	push   %rbp                       
+  400efd:	53                   	push   %rbx                       # 这两个需要使用但是没有看到，six函数内
   400efe:	48 83 ec 28          	sub    $0x28,%rsp
-  400f02:	48 89 e6             	mov    %rsp,%rsi
-  400f05:	e8 52 05 00 00       	callq  40145c <read_six_numbers>
-  400f0a:	83 3c 24 01          	cmpl   $0x1,(%rsp)
-  400f0e:	74 20                	je     400f30 <phase_2+0x34>
-  400f10:	e8 25 05 00 00       	callq  40143a <explode_bomb>
-  400f15:	eb 19                	jmp    400f30 <phase_2+0x34>
-  400f17:	8b 43 fc             	mov    -0x4(%rbx),%eax
-  400f1a:	01 c0                	add    %eax,%eax
-  400f1c:	39 03                	cmp    %eax,(%rbx)
-  400f1e:	74 05                	je     400f25 <phase_2+0x29>
-  400f20:	e8 15 05 00 00       	callq  40143a <explode_bomb>
-  400f25:	48 83 c3 04          	add    $0x4,%rbx
-  400f29:	48 39 eb             	cmp    %rbp,%rbx
-  400f2c:	75 e9                	jne    400f17 <phase_2+0x1b>
-  400f2e:	eb 0c                	jmp    400f3c <phase_2+0x40>
-  400f30:	48 8d 5c 24 04       	lea    0x4(%rsp),%rbx
-  400f35:	48 8d 6c 24 18       	lea    0x18(%rsp),%rbp
-  400f3a:	eb db                	jmp    400f17 <phase_2+0x1b>
+  400f02:	48 89 e6             	mov    %rsp,%rsi                  # rsi保存栈指针
+  400f05:	e8 52 05 00 00       	callq  40145c <read_six_numbers>  # 读了六个数字？
+  400f0a:	83 3c 24 01          	cmpl   $0x1,(%rsp)                # 如果 M[rsp] == 1
+  400f0e:	74 20                	je     400f30 <phase_2+0x34>      #   跳到L2，初始化
+  400f10:	e8 25 05 00 00       	callq  40143a <explode_bomb>      #
+  400f15:	eb 19                	jmp    400f30 <phase_2+0x34>      #
+  400f17:	8b 43 fc             	mov    -0x4(%rbx),%eax            # M[rbx - 4] * 2 -> eax    L1
+  400f1a:	01 c0                	add    %eax,%eax                  # 
+  400f1c:	39 03                	cmp    %eax,(%rbx)                # 如果 eax != M[rbx]
+  400f1e:	74 05                	je     400f25 <phase_2+0x29>      #   就爆炸
+  400f20:	e8 15 05 00 00       	callq  40143a <explode_bomb>      #
+  400f25:	48 83 c3 04          	add    $0x4,%rbx                  # rbx + 4 -> rbx
+  400f29:	48 39 eb             	cmp    %rbp,%rbx                  # 如果 rbp != rbx 
+  400f2c:	75 e9                	jne    400f17 <phase_2+0x1b>      #   跳到L1
+  400f2e:	eb 0c                	jmp    400f3c <phase_2+0x40>      # 否则安全
+  400f30:	48 8d 5c 24 04       	lea    0x4(%rsp),%rbx             # rsp + 4 -> rbx           L2
+  400f35:	48 8d 6c 24 18       	lea    0x18(%rsp),%rbp            # rsp + 18 -> rbp
+  400f3a:	eb db                	jmp    400f17 <phase_2+0x1b>      # 跳到L1
   400f3c:	48 83 c4 28          	add    $0x28,%rsp
   400f40:	5b                   	pop    %rbx
   400f41:	5d                   	pop    %rbp
@@ -801,20 +801,20 @@ Disassembly of section .text:
   401452:	bf 08 00 00 00       	mov    $0x8,%edi
   401457:	e8 c4 f7 ff ff       	callq  400c20 <exit@plt>
 
-000000000040145c <read_six_numbers>:
-  40145c:	48 83 ec 18          	sub    $0x18,%rsp
-  401460:	48 89 f2             	mov    %rsi,%rdx
-  401463:	48 8d 4e 04          	lea    0x4(%rsi),%rcx
-  401467:	48 8d 46 14          	lea    0x14(%rsi),%rax
-  40146b:	48 89 44 24 08       	mov    %rax,0x8(%rsp)
-  401470:	48 8d 46 10          	lea    0x10(%rsi),%rax
-  401474:	48 89 04 24          	mov    %rax,(%rsp)
-  401478:	4c 8d 4e 0c          	lea    0xc(%rsi),%r9
-  40147c:	4c 8d 46 08          	lea    0x8(%rsi),%r8
-  401480:	be c3 25 40 00       	mov    $0x4025c3,%esi
+000000000040145c <read_six_numbers>:  # phase_2和6的奇妙过程,%rbx和%rbp在这里# 如果(%rsp) = 1会防止爆炸
+  40145c:	48 83 ec 18          	sub    $0x18,%rsp                      
+  401460:	48 89 f2             	mov    %rsi,%rdx                       # rsi保存当前栈指针+18byte的位置，
+  401463:	48 8d 4e 04          	lea    0x4(%rsi),%rcx                  # rsi+4 -> rcx
+  401467:	48 8d 46 14          	lea    0x14(%rsi),%rax                 # (覆盖了输入)
+  40146b:	48 89 44 24 08       	mov    %rax,0x8(%rsp)                  # rsi+14 -> M[rsp+8]
+  401470:	48 8d 46 10          	lea    0x10(%rsi),%rax                 #
+  401474:	48 89 04 24          	mov    %rax,(%rsp)                     # M[rsp+10] -> M[rsp]
+  401478:	4c 8d 4e 0c          	lea    0xc(%rsi),%r9                   # rsi+12 -> r9
+  40147c:	4c 8d 46 08          	lea    0x8(%rsi),%r8                   # rsi+8 -> r8
+  401480:	be c3 25 40 00       	mov    $0x4025c3,%esi                  # 把我的输入改成了这个蜜汁数字
   401485:	b8 00 00 00 00       	mov    $0x0,%eax
-  40148a:	e8 61 f7 ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  40148f:	83 f8 05             	cmp    $0x5,%eax
+  40148a:	e8 61 f7 ff ff       	callq  400bf0 <__isoc99_sscanf@plt>    # 发生了什么？
+  40148f:	83 f8 05             	cmp    $0x5,%eax                       # 如果%eax > 5可以躲过去 
   401492:	7f 05                	jg     401499 <read_six_numbers+0x3d>
   401494:	e8 a1 ff ff ff       	callq  40143a <explode_bomb>
   401499:	48 83 c4 18          	add    $0x18,%rsp
