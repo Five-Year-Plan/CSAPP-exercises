6cfd3d4677c794cd90fe572df10a8497da115979
factotumKS <1312005925@qq.com>
2018-12-16 02:12:47 +0800

    solve phase3

--- bomb_lab/bomb.asm
+++ bomb_lab/bomb.asm
@@ -299,7 +299,7 @@ Disassembly of section .text:
   400e32:	e8 67 06 00 00       	callq  40149e <read_line> # 调用读取行的函数
   400e37:	48 89 c7             	mov    %rax,%rdi          # 传给phase_1一个参数，肯定是我们输入的字符串
   400e3a:	e8 a1 00 00 00       	callq  400ee0 <phase_1>
-  /400e3f:	e8 80 07 00 00       	callq  4015c4 <phase_defused>
+  400e3f:	e8 80 07 00 00       	callq  4015c4 <phase_defused>
   400e44:	bf a8 23 40 00       	mov    $0x4023a8,%edi
   400e49:	e8 c2 fc ff ff       	callq  400b10 <puts@plt>
   400e4e:	e8 4b 06 00 00       	callq  40149e <read_line> # 类似输入
@@ -380,21 +380,21 @@ Disassembly of section .text:
   400f41:	5d                   	pop    %rbp
   400f42:	c3                   	retq   
 
-0000000000400f43 <phase_3>:
+0000000000400f43 <phase_3>: # "0 207" 有八组答案均可通过
   400f43:	48 83 ec 18          	sub    $0x18,%rsp
-  400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  400f51:	be cf 25 40 00       	mov    $0x4025cf,%esi
-  400f56:	b8 00 00 00 00       	mov    $0x0,%eax
+  400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx               # rsp + 12 -> rcx
+  400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx               # rsp + 8 -> rdx
+  400f51:	be cf 25 40 00       	mov    $0x4025cf,%esi               # "%d %d" -> esi 上面输入六个int，这里好像是两个的意思
+  400f56:	b8 00 00 00 00       	mov    $0x0,%eax                    # 0 -> eax
   400f5b:	e8 90 fc ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  400f60:	83 f8 01             	cmp    $0x1,%eax
-  400f63:	7f 05                	jg     400f6a <phase_3+0x27>
-  400f65:	e8 d0 04 00 00       	callq  40143a <explode_bomb>
-  400f6a:	83 7c 24 08 07       	cmpl   $0x7,0x8(%rsp)
-  400f6f:	77 3c                	ja     400fad <phase_3+0x6a>
-  400f71:	8b 44 24 08          	mov    0x8(%rsp),%eax
-  400f75:	ff 24 c5 70 24 40 00 	jmpq   *0x402470(,%rax,8)
-  400f7c:	b8 cf 00 00 00       	mov    $0xcf,%eax
+  400f60:	83 f8 01             	cmp    $0x1,%eax                    # if eax > 1
+  400f63:	7f 05                	jg     400f6a <phase_3+0x27>        # then 避免爆炸，跳过下一行
+  400f65:	e8 d0 04 00 00       	callq  40143a <explode_bomb>        # (输入两个整数，rax=2)
+  400f6a:	83 7c 24 08 07       	cmpl   $0x7,0x8(%rsp)               # if M[rsp + 8] > 7
+  400f6f:	77 3c                	ja     400fad <phase_3+0x6a>        # then 爆炸()
+  400f71:	8b 44 24 08          	mov    0x8(%rsp),%eax               # M[rsp + 8]-> eax
+  400f75:	ff 24 c5 70 24 40 00 	jmpq   *0x402470(,%rax,8)           # rax含有判断分支的信息，似乎参照是一个long数组
+  400f7c:	b8 cf 00 00 00       	mov    $0xcf,%eax                   # 应该是一个switch语句，分情况修改eax
   400f81:	eb 3b                	jmp    400fbe <phase_3+0x7b>
   400f83:	b8 c3 02 00 00       	mov    $0x2c3,%eax
   400f88:	eb 34                	jmp    400fbe <phase_3+0x7b>
@@ -407,13 +407,13 @@ Disassembly of section .text:
   400f9f:	b8 aa 02 00 00       	mov    $0x2aa,%eax
   400fa4:	eb 18                	jmp    400fbe <phase_3+0x7b>
   400fa6:	b8 47 01 00 00       	mov    $0x147,%eax
-  400fab:	eb 11                	jmp    400fbe <phase_3+0x7b>
+  400fab:	eb 11                	jmp    400fbe <phase_3+0x7b>        # 一堆全都跳到
   400fad:	e8 88 04 00 00       	callq  40143a <explode_bomb>
-  400fb2:	b8 00 00 00 00       	mov    $0x0,%eax
+  400fb2:	b8 00 00 00 00       	mov    $0x0,%eax                    # default
   400fb7:	eb 05                	jmp    400fbe <phase_3+0x7b>
   400fb9:	b8 37 01 00 00       	mov    $0x137,%eax
-  400fbe:	3b 44 24 0c          	cmp    0xc(%rsp),%eax
-  400fc2:	74 05                	je     400fc9 <phase_3+0x86>
+  400fbe:	3b 44 24 0c          	cmp    0xc(%rsp),%eax               # 大家都往这里跳，if eax == M[rsp + 12]
+  400fc2:	74 05                	je     400fc9 <phase_3+0x86>        # then 避免爆炸，跳过下一行
   400fc4:	e8 71 04 00 00       	callq  40143a <explode_bomb>
   400fc9:	48 83 c4 18          	add    $0x18,%rsp
   400fcd:	c3                   	retq   
