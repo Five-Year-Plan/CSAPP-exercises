e967e32eb8333323360fc7916571f8a6f5c81618
factotumKS <1312005925@qq.com>
2018-12-22 00:57:31 +0800

    solve phase4 in boomb lab

--- bomb_lab/bomb.asm
+++ bomb_lab/bomb.asm
@@ -380,7 +380,7 @@ Disassembly of section .text:
   400f41:	5d                   	pop    %rbp
   400f42:	c3                   	retq   
 
-0000000000400f43 <phase_3>: # "0 207" 有八组答案均可通过
+0000000000400f43 <phase_3>: # "0 207" "2 707" "3 256" ... 有八组答案均可通过 ...
   400f43:	48 83 ec 18          	sub    $0x18,%rsp
   400f47:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx               # rsp + 12 -> rcx
   400f4c:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx               # rsp + 8 -> rdx
@@ -411,57 +411,57 @@ Disassembly of section .text:
   400fad:	e8 88 04 00 00       	callq  40143a <explode_bomb>
   400fb2:	b8 00 00 00 00       	mov    $0x0,%eax                    # default
   400fb7:	eb 05                	jmp    400fbe <phase_3+0x7b>
-  400fb9:	b8 37 01 00 00       	mov    $0x137,%eax
+  400fb9:	b8 37 01 00 00       	mov    $0x137,%eax                  # 怎么肥四？
   400fbe:	3b 44 24 0c          	cmp    0xc(%rsp),%eax               # 大家都往这里跳，if eax == M[rsp + 12]
   400fc2:	74 05                	je     400fc9 <phase_3+0x86>        # then 避免爆炸，跳过下一行
   400fc4:	e8 71 04 00 00       	callq  40143a <explode_bomb>
   400fc9:	48 83 c4 18          	add    $0x18,%rsp
   400fcd:	c3                   	retq   
 
-0000000000400fce <func4>:
+0000000000400fce <func4>:                                   # 期待这个函数把eax修改为0，这时程序正确
   400fce:	48 83 ec 08          	sub    $0x8,%rsp
-  400fd2:	89 d0                	mov    %edx,%eax
-  400fd4:	29 f0                	sub    %esi,%eax
-  400fd6:	89 c1                	mov    %eax,%ecx
-  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx
-  400fdb:	01 c8                	add    %ecx,%eax
-  400fdd:	d1 f8                	sar    %eax
-  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx
-  400fe2:	39 f9                	cmp    %edi,%ecx
-  400fe4:	7e 0c                	jle    400ff2 <func4+0x24>
-  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx
-  400fe9:	e8 e0 ff ff ff       	callq  400fce <func4>
-  400fee:	01 c0                	add    %eax,%eax
-  400ff0:	eb 15                	jmp    401007 <func4+0x39>
-  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax
-  400ff7:	39 f9                	cmp    %edi,%ecx
-  400ff9:	7d 0c                	jge    401007 <func4+0x39>
-  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi
-  400ffe:	e8 cb ff ff ff       	callq  400fce <func4>
-  401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax
+  400fd2:	89 d0                	mov    %edx,%eax            # a = c
+  400fd4:	29 f0                	sub    %esi,%eax            # a = a - s
+  400fd6:	89 c1                	mov    %eax,%ecx            # 
+  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx           # 
+  400fdb:	01 c8                	add    %ecx,%eax            # a = a + a >> 31     奇+1偶+0
+  400fdd:	d1 f8                	sar    %eax                 # a >>= 1             只右边移一位
+  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx   # c = a + s
+  400fe2:	39 f9                	cmp    %edi,%ecx            # if c <= 输入1
+  400fe4:	7e 0c                	jle    400ff2 <func4+0x24>  # then     ------------------>
+  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx      # else func4()
+  400fe9:	e8 e0 ff ff ff       	callq  400fce <func4>       # -----------------------------
+  400fee:	01 c0                	add    %eax,%eax            # a *= 2
+  400ff0:	eb 15                	jmp    401007 <func4+0x39>  # 直接结束
+  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax            # a = 0    <------------------/
+  400ff7:	39 f9                	cmp    %edi,%ecx            # if c >= 输入1 
+  400ff9:	7d 0c                	jge    401007 <func4+0x39>  # then 顺利结束，所以当c=输入1时就可以了，算出来是7
+  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi       # else s = c + 1
+  400ffe:	e8 cb ff ff ff       	callq  400fce <func4>       # func4()
+  401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax # a = 2 * a + 1
   401007:	48 83 c4 08          	add    $0x8,%rsp
   40100b:	c3                   	retq   
 
-000000000040100c <phase_4>:
+000000000040100c <phase_4>: # "7 0" 第一个分析func4，第二个直接看phase_4就行了，暴露很明显
   40100c:	48 83 ec 18          	sub    $0x18,%rsp
-  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi
-  40101f:	b8 00 00 00 00       	mov    $0x0,%eax
+  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx         # 0 -> rcx
+  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx         # 0 -> rdx
+  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi         # addr -> esi 输入地址
+  40101f:	b8 00 00 00 00       	mov    $0x0,%eax              # 0 -> eax
   401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  401029:	83 f8 02             	cmp    $0x2,%eax
-  40102c:	75 07                	jne    401035 <phase_4+0x29>
-  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)
-  401033:	76 05                	jbe    40103a <phase_4+0x2e>
+  401029:	83 f8 02             	cmp    $0x2,%eax              # if eax ！= 2 应该又是几个字符串，这回是2个
+  40102c:	75 07                	jne    401035 <phase_4+0x29>  # then BOOM!!!
+  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)         # elif M[rsp + 8] <= 14 发现这是输入的第一个数 
+  401033:	76 05                	jbe    40103a <phase_4+0x2e>  # then SAFE... for a while
   401035:	e8 00 04 00 00       	callq  40143a <explode_bomb>
-  40103a:	ba 0e 00 00 00       	mov    $0xe,%edx
-  40103f:	be 00 00 00 00       	mov    $0x0,%esi
-  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi
-  401048:	e8 81 ff ff ff       	callq  400fce <func4>
-  40104d:	85 c0                	test   %eax,%eax
-  40104f:	75 07                	jne    401058 <phase_4+0x4c>
-  401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp)
-  401056:	74 05                	je     40105d <phase_4+0x51>
+  40103a:	ba 0e 00 00 00       	mov    $0xe,%edx              # 14 -> edx
+  40103f:	be 00 00 00 00       	mov    $0x0,%esi              # 0 -> esi
+  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi         # 输入的第一个数 -> edi
+  401048:	e8 81 ff ff ff       	callq  400fce <func4>         # 应该是判断两个输入的函数
+  40104d:	85 c0                	test   %eax,%eax              # if eax != 0
+  40104f:	75 07                	jne    401058 <phase_4+0x4c>  # then BOOM!!! 应该是检查判断结果
+  401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp)         # elif M[rsp + 12] == 0 第二个数必然是0
+  401056:	74 05                	je     40105d <phase_4+0x51>  # then SAFE!!!
   401058:	e8 dd 03 00 00       	callq  40143a <explode_bomb>
   40105d:	48 83 c4 18          	add    $0x18,%rsp
   401061:	c3                   	retq   
